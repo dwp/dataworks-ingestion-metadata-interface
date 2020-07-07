ea3bab7664262e5b98b1e1c9f2b930262b7ec6f0
danhill <danhill@digital.uc.dwp.gov.uk>
2020-07-07 16:13:55 +0100

    allow injection and reuse of DB connections
    Signed-off-by: danhill <danhill@digital.uc.dwp.gov.uk>

--- database.py
+++ database.py
@@ -37,16 +37,14 @@ def get_connection():
     )
 
 
-def execute_statement(sql):
-    connection = get_connection()
+def execute_statement(sql, connection=get_connection()):
     cursor = connection.cursor()
     cursor.execute(sql)
     connection.commit()
     connection.close()
 
 
-def execute_query(sql):
-    connection = get_connection()
+def execute_query(sql, connection=get_connection()):
     cursor = connection.cursor()
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -55,9 +53,8 @@ def execute_query(sql):
     return result
 
 
-def execute_file(filename, sql_parameters):
+def execute_file(filename, sql_parameters, connection=get_connection()):
     sql = open(filename).read()
-    connection = get_connection()
     cursor = connection.cursor()
     for result in cursor.execute(sql, sql_parameters, multi=True):
         if result.with_rows:
