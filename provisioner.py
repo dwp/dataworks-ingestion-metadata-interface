a739d2da28138196a521865766cf5316b17d481f
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2020-07-09 15:41:39 +0000

    Fixup: Format Python code with Black

--- provisioner.py
+++ provisioner.py
@@ -55,16 +55,70 @@ def validate_table(database, table_name, connection):
     )
 
     column_structure_required = {
-        'id': {'Field': 'id', 'Type': 'int(11)', 'Null': 'NO', 'Key': 'PRI'},
-        'hbase_id': {'Field': 'hbase_id', 'Type': 'varchar(45)', 'Null': 'YES', 'Key': 'MUL', 'Default': None},
-        'hbase_timestamp': {'Field': 'hbase_timestamp', 'Type': 'datetime', 'Null': 'YES', 'Key': '', 'Default': None},
-        'write_timestamp': {'Field': 'write_timestamp', 'Type': 'datetime', 'Null': 'YES', 'Key': 'MUL', 'Default': None},
-        'correlation_id': {'Field': 'correlation_id', 'Type': 'varchar(45)', 'Null': 'YES', 'Key': '', 'Default': None},
-        'topic_name': {'Field': 'topic_name', 'Type': 'varchar(45)', 'Null': 'YES', 'Key': '', 'Default': None},
-        'kafka_partition': {'Field': 'kafka_partition', 'Type': 'int(11)', 'Null': 'YES', 'Key': '', 'Default': None},
-        'kafka_offset': {'Field': 'kafka_offset', 'Type': 'int(11)', 'Null': 'YES', 'Key': '', 'Default': None},
-        'reconciled_result': {'Field': 'reconciled_result', 'Type': 'tinyint(4)', 'Null': 'NO', 'Key': 'MUL', 'Default': '0'},
-        'reconciled_timestamp': {'Field': 'reconciled_timestamp', 'Type': 'datetime', 'Null': 'YES', 'Key': '', 'Default': None}
+        "id": {"Field": "id", "Type": "int(11)", "Null": "NO", "Key": "PRI"},
+        "hbase_id": {
+            "Field": "hbase_id",
+            "Type": "varchar(45)",
+            "Null": "YES",
+            "Key": "MUL",
+            "Default": None,
+        },
+        "hbase_timestamp": {
+            "Field": "hbase_timestamp",
+            "Type": "datetime",
+            "Null": "YES",
+            "Key": "",
+            "Default": None,
+        },
+        "write_timestamp": {
+            "Field": "write_timestamp",
+            "Type": "datetime",
+            "Null": "YES",
+            "Key": "MUL",
+            "Default": None,
+        },
+        "correlation_id": {
+            "Field": "correlation_id",
+            "Type": "varchar(45)",
+            "Null": "YES",
+            "Key": "",
+            "Default": None,
+        },
+        "topic_name": {
+            "Field": "topic_name",
+            "Type": "varchar(45)",
+            "Null": "YES",
+            "Key": "",
+            "Default": None,
+        },
+        "kafka_partition": {
+            "Field": "kafka_partition",
+            "Type": "int(11)",
+            "Null": "YES",
+            "Key": "",
+            "Default": None,
+        },
+        "kafka_offset": {
+            "Field": "kafka_offset",
+            "Type": "int(11)",
+            "Null": "YES",
+            "Key": "",
+            "Default": None,
+        },
+        "reconciled_result": {
+            "Field": "reconciled_result",
+            "Type": "tinyint(4)",
+            "Null": "NO",
+            "Key": "MUL",
+            "Default": "0",
+        },
+        "reconciled_timestamp": {
+            "Field": "reconciled_timestamp",
+            "Type": "datetime",
+            "Null": "YES",
+            "Key": "",
+            "Default": None,
+        },
     }
 
     for column_name in column_structure_required:
@@ -77,8 +131,10 @@ def validate_table(database, table_name, connection):
                     if key in result_subvalues and value == result_subvalues[key]:
                         pass
                     else:
-                        logger.error(f"{column_name} - {key} has the incorrect structure.")
-                        raise Exception('InvalidColumnStructure')
+                        logger.error(
+                            f"{column_name} - {key} has the incorrect structure."
+                        )
+                        raise Exception("InvalidColumnStructure")
 
             else:
                 logger.error(f"{database}.{table_name} is missing {column_name}")
@@ -87,6 +143,7 @@ def validate_table(database, table_name, connection):
             logger.info(f"{column_name} column has the correct schema settings.")
 
     return True
+
 
 if __name__ == "__main__":
     json_content = json.loads(open("event.json", "r").read())
