f005fbccb627199b861831daf58d2d0253a7dfc0
Connor Avery <connor.avery@infinityworks.com>
2020-07-10 10:02:11 +0100

    Update write_timestamp to be current_timestamp on record creation

--- create_table.sql
+++ create_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `{table_name}` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `hbase_id` VARCHAR(45) NULL,
     `hbase_timestamp` DATETIME NULL,
-    `write_timestamp` DATETIME NULL,
+    `write_timestamp` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `correlation_id` VARCHAR(45) NULL,
     `topic_name` VARCHAR(45) NULL,
     `kafka_partition` INT NULL,
