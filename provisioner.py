b1839288ab96f156faa35b362171f21405be9157
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2020-08-24 11:07:01 +0000

    Fixup: Format Python code with Black

--- provisioner.py
+++ provisioner.py
@@ -22,7 +22,10 @@ def handler(event, context):
     connection = get_connection()
 
     # create table if not exists
-    execute_statement("DROP TABLE {table_name}".format(table_name=Table[args["table-name"]].value), connection)
+    execute_statement(
+        "DROP TABLE {table_name}".format(table_name=Table[args["table-name"]].value),
+        connection,
+    )
     execute_statement(
         open("create_table.sql")
         .read()
