20bc73b37cea5c7eabcd4a0afc1f692a2e5380e1
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2020-09-24 19:32:39 +0100

    Change validation method to use the new column type

--- provisioner.py
+++ provisioner.py
@@ -82,7 +82,7 @@ def validate_table(database, table_name, connection):
         },
         "hbase_timestamp": {
             "Field": "hbase_timestamp",
-            "Type": "datetime",
+            "Type": "bigint",
             "Null": "YES",
             "Key": "",
             "Default": None,
