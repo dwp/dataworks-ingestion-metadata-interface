074b59ac039f7fea7caf9280cd0ae74183a8f570
Mike Winker <mike.winker@dwp.gov.uk>
2020-09-24 14:52:35 +0100

    Drop table for metadata store tables to circumvent transaction timeout

--- provisioner.py
+++ provisioner.py
@@ -22,6 +22,12 @@ def handler(event, context):
     connection = get_connection()
 
     # create table if not exists
+    
+    execute_statement(
+        "DROP TABLE {table_name}".format(table_name=Table[args["table-name"]].value),
+        connection,
+    )
+    
     execute_statement(
         open("create_table.sql")
         .read()
