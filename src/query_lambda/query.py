984b8683c20e56807b6f885f9286a2e602f32847
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2020-09-29 15:39:54 +0100

    The lamdba can't serialise the json back from the result because of the datetime values so this converts them to string, also add lots of logging as it's VERY quiet currently

--- src/query_lambda/query.py
+++ src/query_lambda/query.py
@@ -47,8 +47,13 @@ def handler(event, context):
     if not args_valid:
         raise ValueError("Arguments passed to handler failed to validate")
 
+    logger.info(f"Getting connection to database")
     connection = database.get_connection()
+
+    logger.info(f"Building query")
     query = build_query(args)
+
+    logger.info(f"Getting connection to database")
     result = database.execute_query_to_dict(query, connection)
 
     connection.close()
@@ -57,13 +62,17 @@ def handler(event, context):
 
 
 def build_query(args):
+    global logger
+
     query_connector_type = (
         args["query-connector-type"].upper()
         if "query-connector-type" in args
         else "AND"
     )
-
-    query = f"SELECT * FROM {common.get_table_name(args)}"
+    
+    query = f"SELECT hbase_id, hbase_timestamp, CAST(write_timestamp AS char), "
+        + f"correlation_id, topic_name, kafka_partition, kafka_offset, "
+        + f"reconciled_result, CAST(reconciled_timestamp AS char) FROM {common.get_table_name(args)}"
 
     queryable_options = []
     for queryable_field in queryable_fields:
@@ -84,6 +93,7 @@ def build_query(args):
             for count in range(1, len(queryable_options)):
                 query += f" {query_connector_type} {queryable_options[count]}"
 
+    logger.info(f"Query built as \"{query}\"")
     return query
 
 
