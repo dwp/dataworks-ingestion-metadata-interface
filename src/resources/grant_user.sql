8044b6f97896a46deb575f35da5ffac496d3af7d
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2020-10-19 12:10:23 +0100

    Add missing user that CDL uses and add insert permissions to reconciler so the trimmer can optimise table

--- src/resources/grant_user.sql
+++ src/resources/grant_user.sql
@@ -1,7 +1,9 @@
 CREATE USER IF NOT EXISTS k2hbwriter;
 CREATE USER IF NOT EXISTS reconciler;
 CREATE USER IF NOT EXISTS datareader;
+CREATE USER IF NOT EXISTS hbasewriter;
 
 GRANT SELECT, INSERT ON `{table_name}` to k2hbwriter;
-GRANT SELECT, UPDATE (reconciled_result, reconciled_timestamp) ON `{table_name}` to reconciler;
+GRANT SELECT, INSERT, UPDATE (reconciled_result, reconciled_timestamp) ON `{table_name}` to reconciler;
 GRANT SELECT ON `{table_name}` to datareader;
+GRANT SELECT, INSERT ON `{table_name}` to hbasewriter;
