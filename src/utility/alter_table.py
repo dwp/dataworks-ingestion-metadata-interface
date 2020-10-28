76a23d19f7314c35d61261ae6e8b4050f28ffda0
Daniel Chicot <dan.chicot@gmail.com>
2020-10-28 13:51:40 +0000

    Utility script to exercise the alter table stored procedure.

---
+++ src/utility/alter_table.py
@@ -0,0 +1,27 @@
+import mysql.connector
+import mysql.connector.pooling
+
+
+# Runs the alter reconciliation stored procedure for local dev/testing.
+def main():
+    connection = mysql.connector.connect(host="localhost",
+                                         user="root",
+                                         password="password",
+                                         database="metadatastore")
+    try:
+        script = "../resources/alter_reconciliation_table.sql"
+        contents = open(script).read()
+        print(contents)
+        cursor = connection.cursor()
+        for result in cursor.execute(contents, multi=True):
+            print(result)
+
+        for result in cursor.callproc("alter_reconciliation_table", ["ucfs"]):
+            print(f"Result {result}")
+
+    except mysql.connector.Error as e:
+        print(f"{e}")
+
+
+if __name__ == "__main__":
+    main()
